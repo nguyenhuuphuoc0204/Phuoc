f2a46087279bbcf9d7db446ad6f65ace69d01dcf
Phuoc <phuoc0519@gmail.com>
2024-03-18 14:18:12 +0700

    Neww

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Login from '../PhuocExpo/Screens/Login.tsx'; // Đảm bảo bạn import đúng đường dẫn của component Login
+
+export default function App() {
+  return (
+    <View>
+      <Text>Helaalo</Text>
+      <Login/>
+    </View>
+  );
+}
